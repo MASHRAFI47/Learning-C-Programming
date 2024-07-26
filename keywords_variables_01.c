9419081591fd4c62d53b0d30eb84cb0111a09358
Mashrafi Bin Nur <mbngms@gmail.com>
2024-07-26 20:02:21 +0600

    code added

---
+++ keywords_variables_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int x = 5;
+    float y = 6.44;
+    double z = 5.231512512;
+    char c = 'a';
+    
+    printf("This is int %d\n", x);
+    printf("This is float %f\n", y);
+    printf("This is double int %lf\n", z);
+    printf("This is a char %c", c);
+    return 0;
+}
