f26849bc7a9eea8cf96eb7df0be48117ca293c9c
Mashrafi Bin Nur <mbngms@gmail.com>
2024-07-26 20:13:40 +0600

    more data types

---
+++ more_data_types_03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+    // data types
+    // 1. basic data types: int, float, double, char, void
+    // 2. Derived Data Types: arrays, unions, enum, pointers, struct
+
+    //array
+    int numbers[20]; //this is an array of numbers which containes 20 numbers
+
+    //structure 
+    struct person {
+        char name[50];
+        int age;
+    }
+
+    return 0;
+}
---
+++ take_integer_02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    int num;
+    printf("Please enter an input : ");
+    scanf("%d", &num);
+    printf("Your output is %d\n", num);
+    return 0;
+}
