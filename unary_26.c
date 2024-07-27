b79a7f7049e0168d3bc2b9e6cc1f99b492bb3318
Mashrafi Bin Nur <mbngms@gmail.com>
2024-07-27 18:19:21 +0600

    some more unary

--- unary_26.c
+++ unary_26.c
@@ -2,10 +2,15 @@
 
 int main () {
     int x = 10;
-    int y = x++;
+    int y = x++; //postfix increment
+    
+    int a = 20;
+    int b = ++a; //prefix increment
 
     printf("value of x: %d\n", x);
     printf("value of y: %d\n", y);
+    printf("value of a: %d\n", a);
+    printf("value of b: %d\n", b);
 
     return 0;
 }
