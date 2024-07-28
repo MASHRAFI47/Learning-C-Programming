e516826c58678885ec7c8060fb0b386a8af6a8eb
Mashrafi Bin Nur <mbngms@gmail.com>
2024-07-28 22:44:59 +0600

    multiplication

---
+++ multiplicationTable_39.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+    printf("Please enter a number for the table: ");
+    scanf("%d", &num);
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
