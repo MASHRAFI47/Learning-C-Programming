7f2b2a1e713162cae2e30cb4b5c5b8f2621e9079
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-01 04:45:56 +0600

    series sevenn

--- seriesSeven_57.c
+++ seriesSeven_57.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    int i, n, sum=0, a=1;
+int main()
+{
+    float i, n, sum = 0, a = 1;
     printf("series: 1 + 1/2 + 1/3 + .... 1/n\n");
     printf("Please enter n: ");
-    scanf("%d", &n);
+    scanf("%f", &n);
 
-    for (i = 1; i <=n ; i++)
+    for (i = 1; i <= n; i++)
     {
         sum = sum + (1 / i);
+        
+
+        if (i == 1)
+        {
+            printf("\n 1 + ");
+        }
+        else if (i == n)
+        {
+            printf("(1/%.2f)", i);
+        }
+        else
+        {
+            printf("(1/%.2f) + ", i);
+        }
     }
-    
-    printf("The sum of series: %f", sum);
+
+    printf(" = %.2f\n", sum);
 }
