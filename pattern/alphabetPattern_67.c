7f0240eb6893d343498c9855b98f511a2f01c618
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-01 22:39:49 +0600

    alphabet pattern two

---
+++ pattern/alphabetPattern_67.c
@@ -0,0 +1,25 @@
+/*
+    A
+    B B
+    C C C
+    D D D D
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int n, row, col;
+    printf("Enter range: ");
+    scanf("%d", &n);
+
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            printf("%c", row + 64);
+        }
+        printf("\n");
+    }
+    
+}
