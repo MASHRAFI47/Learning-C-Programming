5d7645dcbe8206b9186a073293d109e1de257074
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-01 22:13:01 +0600

    binary pattern

---
+++ pattern/binaryPattern_64.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    while (1)
+    {
+        int n, row, col;
+        printf("Please type n: ");
+        scanf("%d", &n);
+
+        for (row = 1; row <= n; row++)
+        {
+            for (col = 1; col <= row; col++)
+            {
+                printf("%d ", col % 2);
+            }
+            printf("\n");
+        }
+    }
+}
