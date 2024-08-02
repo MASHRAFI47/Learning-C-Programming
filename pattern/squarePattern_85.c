e9f60943fb8807d96bdc2e7d394133523068f7e9
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-02 23:32:45 +0600

    square pattern

---
+++ pattern/squarePattern_85.c
@@ -0,0 +1,25 @@
+/*
+   1234
+   1234
+   1234
+   1234
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, row, col;
+
+    printf("Enter range: ");
+    scanf("%d", &n);
+
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= n; col++)
+        {
+            printf("%d", col);
+        }
+        printf("\n");
+    }
+}
