3f646150974998d6b64c464bccb7cbbf8e0aa809
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-03 02:17:40 +0600

    space triangle

---
+++ pattern/spaceTriangle_93.c
@@ -0,0 +1,54 @@
+/*
+    * 
+   * * 
+  * * * 
+ * * * * 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, row, col;
+
+    printf("Enter range: ");
+    scanf("%d", &n);
+
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= n - row; col++)
+        {
+            printf(" ");
+        }
+        for (col = 1; col <= row; col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+
+// #include <stdio.h>
+
+// int main () {
+//     int n, row, col;
+    
+//     printf("Enter range: ");
+//     scanf("%d", &n);
+    
+//     for(row = 1; row <= n; row++) {
+//         for(col = 1; col <= n-row; col++) {
+//             printf(" ");
+//         }
+//         for(col = 1; col <= row*2 -1; col++) {
+//             if(col%2==0) {
+//                 printf(" ");
+//             }
+//             else {
+//                 printf("%d", col);
+//             }
+//         }
+//         printf("\n");
+//     }
+// }
