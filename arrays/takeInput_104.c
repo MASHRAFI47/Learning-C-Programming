9b9f3f5d84d5c78e9059291817a0b49cb19ee78e
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-04 04:12:25 +0600

    array inputs

---
+++ arrays/takeInput_104.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter number: ");
+        scanf("%d", &numbers[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", numbers[i]);
+        printf("\n");
+    }
+
+    return 0;
+}
