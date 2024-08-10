2ae12b5e5e1c69e693160e7a1bd603514de71284
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-11 02:06:08 +0600

    recursion

---
+++ functions/recursion_146.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+
+int main()
+{
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    int result = fact(num);
+    printf("fact of %d is: %d", num, result);
+}
