38ff67127bc0b59d3071cd59ebe46ffc02baae96
Mashrafi Bin Nur <mbngms@gmail.com>
2024-08-11 00:59:55 +0600

    passing String

---
+++ functions/passingString_145.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void display(char str[])
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        printf("%c\n", str[i]);
+        i++;
+    }
+}
+
+int main()
+{
+    char str[] = "Programming";
+
+    display(str);
+}
